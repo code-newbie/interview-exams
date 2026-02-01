e69e77182139d50130ffbcbd448c864e92799103
A.Rachmat <ananda_rachmat@yahoo.com>
2026-02-01 23:56:55 +0700

    Add initial working SPRING projects

---
+++ src/main/java/net/anandarachmat/xtramile_exams/XtramileExamsApplication.java
@@ -0,0 +1,13 @@
+package net.anandarachmat.xtramile_exams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtramileExamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtramileExamsApplication.class, args);
+	}
+
+}
